e3b88a84469090e2c4d3cd36a78d6618cafeddce
Max Lewis <max.a.lewis98@gmail.com>
2020-05-10 00:10:24 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// constructor functions
+
+// mines
+    // add value to adjacent empty plots
+    // onLeftclick explode and lose game
+
+// plots
+    // grey on spawn
+    // onleftclick uncovers number
+
+// 0 plots
+    // uncover all 8 plots around them when uncovered
+    // if uncovered plot is a 0 plot uncover the next 8 plots
+
+
+// generate game function
+    // dificulty switch statement
+    // math.random lat and long and place mines on a grid
+    // when mines get placed they add +1 value to 8 adjecent non-mines
+    // give the player total mines and flag count and send them off
+
+$(window).ready(() => {
+
+
+
+})
